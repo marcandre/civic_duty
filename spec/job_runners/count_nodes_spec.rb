929db14068bdfe98e814536cd75b119a1c91694d
Marc-Andre Lafortune <github@marc-andre.ca>
2019-03-13 00:55:16 -0400

    Refactor

--- spec/job_runners/count_nodes_spec.rb
+++ spec/job_runners/count_nodes_spec.rb
@@ -3,19 +3,22 @@ require_relative '../spec_helper'
 module CivicDuty
   RSpec.describe CountNodes do
     let(:runner_class) { described_class }
-    let(:params) { {matcher: Matcher::Send.new(:autoload)} }
     let(:repository) { ManualRepository.create! name: 'trivial_gem' }
     let(:projects) { Project.create! repository: repository }
-    let(:job) { Job.create!(runner_class: runner_class, params: params)
-                .create_tasks_for(projects)
-                .run }
-    let(:task) { job.tasks.first }
-    subject { task }
 
-    it 'should be a success' do
-      puts task.builds.last.output unless task.status == :success
-      task.status.should eq :success
+    describe 'from a Job created manually' do
+      let(:params) { {matcher: Matcher::Send.new(:autoload)} }
+      let(:job) { Job.create!(runner_class: runner_class, params: params)
+                  .create_tasks_for(projects)
+                  .run }
+      let(:task) { job.tasks.first }
+      subject { task }
+
+      it 'should be a success' do
+        puts task.builds.last.output unless task.status == :success
+        task.status.should eq :success
+      end
+      its(:step_result) { should eq 3 }
     end
-    its(:step_result) { should eq 3 }
   end
 end

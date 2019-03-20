e8d7d706432c2f3b5b04e5d051f817948a52b027
Marc-Andre Lafortune <github@marc-andre.ca>
2019-03-20 15:13:35 -0400

    This has been released in RuboCop 0.66

--- lib/civic_duty/ext/node_pattern.rb
+++ lib/civic_duty/ext/node_pattern.rb
@@ -1,36 +1,6 @@
 module CivicDuty
   module Ext
     module NodePattern
-      attr_reader :pattern
-
-      def initialize(str)
-        @pattern = str
-        super
-      end
-
-      def match(*args)
-        # If we're here, it's because the singleton method has not been defined,
-        # either because we've been dup'ed or serialized through YAML
-        initialize(pattern)
-        match(*args)
-      end
-
-      def marshal_load(pattern)
-        initialize pattern
-      end
-
-      def marshal_dump
-        pattern
-      end
-
-      def ==(other)
-        other.is_a?(::RuboCop::NodePattern) && other.pattern == pattern
-      end
-      alias eql? ==
-
-      def to_s
-        "#<#{self.class} #{pattern}>"
-      end
     end
   end
 end

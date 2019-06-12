991f71000003acc9e485886c27786bc1c949f6db
Marc-Andre Lafortune <github@marc-andre.ca>
2019-06-12 22:32:25 +0200

    Sort pathnames

--- lib/civic_duty/step_helpers/count_nodes.rb
+++ lib/civic_duty/step_helpers/count_nodes.rb
@@ -14,7 +14,8 @@ module CivicDuty
 
     private def each_ruby_file(dir, &block)
       return to_enum __method__, dir unless block_given?
-      Pathname.glob(dir.join('**/*.rb')).select(&:file?).each(&block)
+      Pathname.glob(dir.join('**/*.rb')).select(&:file?).sort
+      .each(&block)
       self
     end
   end
